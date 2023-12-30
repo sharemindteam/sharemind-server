ed6e229d518eecc54a19d0109ded9f6edacfbb99
aeyongdodam <kikititi0510@gmail.com>
2023-12-30 09:05:57 +0900

    init: 초기 환경 구성

---
+++ src/main/java/com/example/sharemind/SharemindApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharemindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sharemind/SharemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
