f0be08abb89719333c80642e0700a9a8a55a1624
letskuku <yujung9288@naver.com>
2023-12-31 22:03:42 +0900

    feat: CustomerRepository 추가

---
+++ src/main/java/com/example/sharemind/customer/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.sharemind.customer.repository;
+
+import com.example.sharemind.customer.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
