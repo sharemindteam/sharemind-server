db64c7e17d5d0dfc9fdfe00a4c2642f15c411abe
letskuku <yujung9288@naver.com>
2023-12-31 22:58:29 +0900

    feat: CustomUserDetails, CustomUserDetailsService 구현

--- src/main/java/com/example/sharemind/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/sharemind/customer/repository/CustomerRepository.java
@@ -4,7 +4,10 @@ import com.example.sharemind.customer.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Boolean existsByEmail(String email);
+    Optional<Customer> findByEmail(String email);
 }
---
+++ src/main/java/com/example/sharemind/global/jwt/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.example.sharemind.global.jwt;
+
+import com.example.sharemind.customer.domain.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Customer customer;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return customer.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.toString()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return customer.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return customer.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return customer.isActivated();
+    }
+}
