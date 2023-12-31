a4e3600771215bb46e2dc59d0caf5d9a2a700258
letskuku <yujung9288@naver.com>
2023-12-31 23:05:15 +0900

    feat: 커스텀 예외 추가

--- src/main/java/com/example/sharemind/customer/exception/CustomerErrorCode.java
+++ src/main/java/com/example/sharemind/customer/exception/CustomerErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CustomerErrorCode {
 
-    EMAIL_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 회원으로 등록된 이메일입니다.");
+    EMAIL_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 회원으로 등록된 이메일입니다."),
+    CUSTOMER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보가 존재하지 않습니다.");
 
     private final HttpStatus errorHttpStatus;
     private String errorMessage;
--- src/main/java/com/example/sharemind/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/sharemind/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.sharemind.global.exception;
 
 import com.example.sharemind.customer.exception.CustomerException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,5 +16,12 @@ public class GlobalExceptionHandler {
         log.error(e.getErrorCode().getErrorMessage(), e);
         return ResponseEntity.status(e.getErrorCode().getErrorHttpStatus())
                 .body(GlobalExceptionResponse.of(e.getErrorCode().getErrorHttpStatus(), e.getErrorCode().getErrorMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GlobalExceptionResponse> catchException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(GlobalExceptionResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
     }
 }
