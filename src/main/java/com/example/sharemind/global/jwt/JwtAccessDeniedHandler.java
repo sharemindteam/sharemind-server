0e547f60f7ddcd58a524e791bf3022b363e34e75
letskuku <yujung9288@naver.com>
2023-12-31 23:00:42 +0900

    feat: JwtAuthenticationFilter 및 인증인가 예외 처리 구현

---
+++ src/main/java/com/example/sharemind/global/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package com.example.sharemind.global.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("Forbidden Request : {}", request.getRequestURI());
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
+    }
+}
---
+++ src/main/java/com/example/sharemind/global/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.example.sharemind.global.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized Request : {}", request.getRequestURI());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "사용자 인증이 필요합니다.");
+    }
+}
