cd98711ce0c50c895c86eb189046c3c181505777
letskuku <yujung9288@naver.com>
2023-12-31 23:04:45 +0900

    feat: 로그인 비즈니스 로직 구현

--- src/main/java/com/example/sharemind/auth/application/AuthService.java
+++ src/main/java/com/example/sharemind/auth/application/AuthService.java
@@ -1,7 +1,10 @@
 package com.example.sharemind.auth.application;
 
+import com.example.sharemind.auth.dto.request.AuthSignInRequest;
 import com.example.sharemind.auth.dto.request.AuthSignUpRequest;
+import com.example.sharemind.auth.dto.response.TokenDto;
 
 public interface AuthService {
     void signUp(AuthSignUpRequest authSignUpRequest);
+    TokenDto signIn(AuthSignInRequest authSignInRequest);
 }
--- src/main/java/com/example/sharemind/auth/application/AuthServiceImpl.java
+++ src/main/java/com/example/sharemind/auth/application/AuthServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.sharemind.auth.application;
 
+import com.example.sharemind.auth.dto.request.AuthSignInRequest;
 import com.example.sharemind.auth.dto.request.AuthSignUpRequest;
+import com.example.sharemind.auth.dto.response.TokenDto;
 import com.example.sharemind.customer.domain.Customer;
 import com.example.sharemind.customer.exception.CustomerErrorCode;
 import com.example.sharemind.customer.exception.CustomerException;
 import com.example.sharemind.customer.repository.CustomerRepository;
+import com.example.sharemind.global.common.BaseEntity;
+import com.example.sharemind.global.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
+    private final TokenProvider tokenProvider;
     private final PasswordEncoder passwordEncoder;
     private final CustomerRepository customerRepository;
 
@@ -28,5 +34,23 @@ public class AuthServiceImpl implements AuthService {
 
         Customer customer = authSignUpRequest.toEntity(passwordEncoder.encode(authSignUpRequest.getPassword()));
         customerRepository.save(customer);
+    }
+
+    @Transactional
+    @Override
+    public TokenDto signIn(AuthSignInRequest authSignInRequest) {
+
+        Customer customer = customerRepository.findByEmail(authSignInRequest.getEmail())
+                .filter(BaseEntity::isActivated)
+                .orElseThrow(() -> new CustomerException(CustomerErrorCode.CUSTOMER_NOT_FOUND, authSignInRequest.getEmail()));
+
+        if (!passwordEncoder.matches(authSignInRequest.getPassword(), customer.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String accessToken = tokenProvider.createAccessToken(customer.getEmail(), customer.getRoles());
+        String refreshToken = tokenProvider.createRefreshToken(customer.getEmail());
+
+        return TokenDto.of(accessToken, refreshToken);
     }
 }
