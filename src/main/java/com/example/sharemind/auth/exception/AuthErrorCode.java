fcd147159a32aaae4a6aa8ad60b87a5e95261258
letskuku <yujung9288@naver.com>
2024-01-04 02:59:15 +0900

    #3 refactor: refreshToken 에러 메시지 명확화

--- src/main/java/com/example/sharemind/auth/exception/AuthErrorCode.java
+++ src/main/java/com/example/sharemind/auth/exception/AuthErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorCode {
 
     ILLEGAL_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-    ILLEGAL_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "refresh token이 올바르지 않습니다.");
+    ILLEGAL_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "refresh token이 이미 만료되었거나 올바르지 않습니다.");
 
     private final HttpStatus errorHttpStatus;
     private String errorMessage;
