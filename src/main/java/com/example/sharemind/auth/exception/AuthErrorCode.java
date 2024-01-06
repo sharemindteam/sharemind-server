c3d16dece29fe4a97d06867de7b431067fcbe4e7
letskuku <yujung9288@naver.com>
2024-01-06 22:43:19 +0900

    #8 refactor: 커스텀 예외 네이밍 수정

--- src/main/java/com/example/sharemind/auth/exception/AuthErrorCode.java
+++ src/main/java/com/example/sharemind/auth/exception/AuthErrorCode.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum AuthErrorCode {
 
-    ILLEGAL_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-    ILLEGAL_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "refresh token이 이미 만료되었거나 올바르지 않습니다.");
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "refresh token이 이미 만료되었거나 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
