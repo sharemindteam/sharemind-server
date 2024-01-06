5eebd4167884adae0e76101240176cc57834b858
aeyongdodam <kikititi0510@gmail.com>
2024-01-07 03:50:47 +0900

    #4 feat: repository 어노테이션 추가

--- src/main/java/com/example/sharemind/consult/repository/ConsultRepository.java
+++ src/main/java/com/example/sharemind/consult/repository/ConsultRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsultRepository extends JpaRepository<Consult, Long> {
     @Query("SELECT realtime.realtimeId FROM Consult c JOIN c.realtimeConsult realtime WHERE c.customer.customerId = :customerId")
     List<Long> findRealtimeConsultIdsByCustomerId(@Param("customerId") Long customerId); //todo: 쿼리 최적화 필요
