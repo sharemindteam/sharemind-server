9088ebdcdb359f0a122ca63390d2d6897ec19285
letskuku <yujung9288@naver.com>
2024-01-07 04:03:43 +0900

    #10 feat: 결제 및 활성화 여부로 상담 리스트 조회하는 메서드 추가

--- src/main/java/com/example/sharemind/consult/repository/ConsultRepository.java
+++ src/main/java/com/example/sharemind/consult/repository/ConsultRepository.java
@@ -4,6 +4,9 @@ import com.example.sharemind.consult.domain.Consult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConsultRepository extends JpaRepository<Consult, Long> {
+    List<Consult> findAllByIsPaidIsFalseAndIsActivatedIsTrue();
 }
