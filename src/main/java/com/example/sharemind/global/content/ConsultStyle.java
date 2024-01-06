2098ef0b59edb4a83b4dcad07ed2dcda106dd4bb
letskuku <yujung9288@naver.com>
2024-01-07 02:31:19 +0900

    #8 feat: ConsultStyle, ConsultCategory에 displayName 필드 추가

--- src/main/java/com/example/sharemind/global/content/ConsultStyle.java
+++ src/main/java/com/example/sharemind/global/content/ConsultStyle.java
@@ -1,7 +1,16 @@
 package com.example.sharemind.global.content;
 
+import lombok.Getter;
+
+@Getter
 public enum ConsultStyle {
-    SYMPATHY,
-    ADVICE,
-    FACT
+    SYMPATHY("공감"),
+    ADVICE("조언"),
+    FACT("팩폭");
+
+    private final String displayName;
+
+    ConsultStyle(String displayName) {
+        this.displayName = displayName;
+    }
 }
