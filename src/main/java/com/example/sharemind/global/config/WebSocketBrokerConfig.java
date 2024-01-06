2fb197176bc588dae874f8a8d2f31a1156303d52
aeyongdodam <kikititi0510@gmail.com>
2024-01-06 16:38:26 +0900

    #4 feat: websocketConfig 파일 작성

---
+++ src/main/java/com/example/sharemind/global/config/WebSocketBrokerConfig.java
@@ -0,0 +1,23 @@
+package com.example.sharemind.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker(("/queue"));
+        registry.setApplicationDestinationPrefixes(("/app"));
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS(); //todo: 현재 테스트를 위해 해제하는 경우가 있어 AllowedOriginPatterns를 사용, 추후 시큐리티를 씌웠을 때 정상 동작하는지 웹소켓 완성 후 알아봐야함
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
