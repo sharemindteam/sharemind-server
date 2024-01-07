da46664a88c67488953d0a321914d74efbbc0369
aeyongdodam <kikititi0510@gmail.com>
2024-01-07 15:01:22 +0900

    #4 feat: ChattingResponse dto 생성

---
+++ src/main/java/com/example/sharemind/realtimeConsult/dto/response/ChattingResponse.java
@@ -0,0 +1,25 @@
+package com.example.sharemind.realtimeConsult.dto.response;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChattingResponse {
+    private final String senderName;
+    private final String content;
+    private final String sendTime;
+
+    private ChattingResponse(String senderName, String content, LocalDateTime sendTime) {
+        this.senderName = senderName;
+        this.content = content;
+        this.sendTime = sendTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public static ChattingResponse from(String content, String senderName) {
+        return new ChattingResponse(senderName, content, LocalDateTime.now());
+    }
+}
