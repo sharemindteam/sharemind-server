0621bf15c319ee08b3e762d24531e7c6967f4ce6
letskuku <yujung9288@naver.com>
2024-01-09 03:35:11 +0900

    #13 feat: 미결제 상담 결제 완료 여부 수정 시 비실시간 상담 생성하는 로직 추가

--- src/main/java/com/example/sharemind/admin/application/AdminServiceImpl.java
+++ src/main/java/com/example/sharemind/admin/application/AdminServiceImpl.java
@@ -2,9 +2,8 @@ package com.example.sharemind.admin.application;
 
 import com.example.sharemind.admin.dto.response.ConsultsGetUnpaidResponse;
 import com.example.sharemind.consult.domain.Consult;
-import com.example.sharemind.consult.exception.ConsultErrorCode;
-import com.example.sharemind.consult.exception.ConsultException;
-import com.example.sharemind.consult.repository.ConsultRepository;
+import com.example.sharemind.nonRealtimeConsult.application.NonRealtimeConsultService;
+import com.example.sharemind.nonRealtimeConsult.domain.NonRealtimeConsult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +15,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService {
 
-    private final ConsultRepository consultRepository;
+    private final ConsultService consultService;
+    private final NonRealtimeConsultService nonRealtimeConsultService;
 
     @Override
     public List<ConsultsGetUnpaidResponse> getUnpaidConsults() {
@@ -33,11 +33,13 @@ public class AdminServiceImpl implements AdminService {
         consult.updateIsPaid();
 
         // TODO 상담 유형에 따라 비실시간/실시간 상담 생성
-        /**
-         * switch (consult.getConsultType()) {
-         *             case REALTIME ->
-         *             case NON_REALTIME ->
-         *         }
-         */
+        switch (consult.getConsultType()) {
+            // case REALTIME -> consult.updateIsPaidAndRealtimeConsult(realtimeConsult)
+            case NON_REALTIME -> {
+                NonRealtimeConsult nonRealtimeConsult = nonRealtimeConsultService.createNonRealtimeConsult();
+
+                consult.updateIsPaidAndNonRealtimeConsult(nonRealtimeConsult);
+            }
+        }
     }
 }
