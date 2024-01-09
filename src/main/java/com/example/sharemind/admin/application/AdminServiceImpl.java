7010ace9f7233bca5e8e915f540b88ca2208dcf0
letskuku <yujung9288@naver.com>
2024-01-10 02:24:28 +0900

    #13 feat: 결제 완료 여부 수정 시 미결제 상담인지 확인하는 로직 추가

--- src/main/java/com/example/sharemind/admin/application/AdminServiceImpl.java
+++ src/main/java/com/example/sharemind/admin/application/AdminServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.sharemind.admin.application;
 import com.example.sharemind.admin.dto.response.ConsultsGetUnpaidResponse;
 import com.example.sharemind.consult.application.ConsultService;
 import com.example.sharemind.consult.domain.Consult;
+import com.example.sharemind.consult.exception.ConsultErrorCode;
+import com.example.sharemind.consult.exception.ConsultException;
 import com.example.sharemind.nonRealtimeConsult.application.NonRealtimeConsultService;
 import com.example.sharemind.nonRealtimeConsult.domain.NonRealtimeConsult;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +32,9 @@ public class AdminServiceImpl implements AdminService {
     public void updateIsPaid(Long consultId) {
 
         Consult consult = consultService.getConsultByConsultId(consultId);
+        if (consult.getIsPaid()) {
+            throw new ConsultException(ConsultErrorCode.CONSULT_ALREADY_PAID, consultId.toString());
+        }
 
         // TODO 상담 유형에 따라 비실시간/실시간 상담 생성
         switch (consult.getConsultType()) {
--- src/main/java/com/example/sharemind/consult/exception/ConsultErrorCode.java
+++ src/main/java/com/example/sharemind/consult/exception/ConsultErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum ConsultErrorCode {
 
     CONSULT_NOT_FOUND(HttpStatus.NOT_FOUND, "상담 정보가 존재하지 않습니다."),
-    CONSULT_TYPE_MISMATCH(HttpStatus.CONFLICT, "상담 유형과 일치하지 않습니다.");
+    CONSULT_TYPE_MISMATCH(HttpStatus.CONFLICT, "상담 유형과 일치하지 않습니다."),
+    CONSULT_ALREADY_PAID(HttpStatus.BAD_REQUEST, "이미 결제 완료된 상담입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
