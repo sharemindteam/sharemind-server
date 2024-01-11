68a1d7b13cefaba376eeae41e85b7baf05e909d7
letskuku <yujung9288@naver.com>
2024-01-11 12:28:49 +0900

    #16 feat: LetterMessage 수정 api 구현

--- src/main/java/com/example/sharemind/nonRealtimeMessage/presentation/LetterMessageController.java
+++ src/main/java/com/example/sharemind/nonRealtimeMessage/presentation/LetterMessageController.java
@@ -3,18 +3,16 @@ package com.example.sharemind.nonRealtimeMessage.presentation;
 import com.example.sharemind.global.jwt.CustomUserDetails;
 import com.example.sharemind.nonRealtimeMessage.application.LetterMessageService;
 import com.example.sharemind.nonRealtimeMessage.dto.request.LetterMessageCreateRequest;
+import com.example.sharemind.nonRealtimeMessage.dto.request.LetterMessageUpdateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/nonRealtimeMessages")
+@RequestMapping("/api/v1/letterMessages")
 @RequiredArgsConstructor
 public class LetterMessageController {
 
@@ -25,5 +23,12 @@ public class LetterMessageController {
                                                          @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         letterMessageService.createLetterMessage(letterMessageCreateRequest, customUserDetails.getCustomer());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updateLetterMessage(@Valid @RequestBody LetterMessageUpdateRequest letterMessageUpdateRequest,
+                                                    @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        letterMessageService.updateLetterMessage(letterMessageUpdateRequest, customUserDetails.getCustomer());
+        return ResponseEntity.ok().build();
     }
 }
