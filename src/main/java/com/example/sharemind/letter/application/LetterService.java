1b4290cf421aa594813edd7aba5d0c551ceeb387
letskuku <yujung9288@naver.com>
2024-01-12 01:41:03 +0900

    #16 feat: 편지에 대한 상담사 카테고리 조회하는 비즈니스 로직 구현

--- src/main/java/com/example/sharemind/letter/application/LetterService.java
+++ src/main/java/com/example/sharemind/letter/application/LetterService.java
@@ -1,9 +1,12 @@
 package com.example.sharemind.letter.application;
 
 import com.example.sharemind.letter.domain.Letter;
+import com.example.sharemind.letter.dto.response.LetterGetCounselorCategoriesResponse;
 
 public interface LetterService {
     Letter createLetter();
 
     Letter getLetterByLetterId(Long letterId);
+
+    LetterGetCounselorCategoriesResponse getCounselorCategories(Long letterId);
 }
