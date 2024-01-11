41364a5c901e8a10932902a73a749abd11bbc805
letskuku <yujung9288@naver.com>
2024-01-12 02:16:27 +0900

    #16 feat: 계층 간 첫번째 질문 정보 이동 위한 dto 구현

--- src/main/java/com/example/sharemind/letterMessage/dto/request/LetterMessageCreateRequest.java
+++ src/main/java/com/example/sharemind/letterMessage/dto/request/LetterMessageCreateRequest.java
@@ -5,9 +5,11 @@ import com.example.sharemind.letterMessage.domain.LetterMessage;
 import com.example.sharemind.letterMessage.content.LetterMessageType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LetterMessageCreateRequest {
 
     @NotNull(message = "편지 id는 공백일 수 없습니다.")
@@ -21,6 +23,11 @@ public class LetterMessageCreateRequest {
 
     @NotNull(message = "작성 완료 여부는 공백일 수 없습니다.")
     private Boolean isCompleted;
+
+    public static LetterMessageCreateRequest of(LetterMessageCreateFirstRequest letterMessageCreateFirstRequest) {
+        return new LetterMessageCreateRequest(letterMessageCreateFirstRequest.getLetterId(), LetterMessageType.FIRST_QUESTION.getDisplayName(),
+                letterMessageCreateFirstRequest.getContent(), letterMessageCreateFirstRequest.getIsCompleted());
+    }
 
     public LetterMessage toEntity(Letter letter, LetterMessageType messageType) {
         return LetterMessage.builder()
