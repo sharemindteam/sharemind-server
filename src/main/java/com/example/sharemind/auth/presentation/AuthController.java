a73be1370a01f69ce161c52bad2bd98948166167
letskuku <yujung9288@naver.com>
2024-01-13 00:30:27 +0900

    #23 refactor: auth 스웨거 설명 추가

--- src/main/java/com/example/sharemind/auth/presentation/AuthController.java
+++ src/main/java/com/example/sharemind/auth/presentation/AuthController.java
@@ -5,6 +5,13 @@ import com.example.sharemind.auth.dto.request.AuthReissueRequest;
 import com.example.sharemind.auth.dto.request.AuthSignInRequest;
 import com.example.sharemind.auth.dto.request.AuthSignUpRequest;
 import com.example.sharemind.auth.dto.response.TokenDto;
+import com.example.sharemind.global.exception.CustomExceptionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth Controller", description = "인증 컨트롤러")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -21,17 +29,53 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "회원가입", description = "customer 생성")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "이미 가입된 이메일 주소",
+                            content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "1. 존재하지 않는 상담 아이디로 요청됨\n 2. 존재하지 않는 후원 아이디로 요청됨",
+                            content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
     @PostMapping("/signUp")
     public ResponseEntity<Void> signUp(@Valid @RequestBody AuthSignUpRequest authSignUpRequest) {
         authService.signUp(authSignUpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "로그인", description = "로그인")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "400", description = "비밀번호 불일치",
+                            content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 이메일로 요청됨",
+                            content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
     @PostMapping("/signIn")
     public ResponseEntity<TokenDto> signIn(@Valid @RequestBody AuthSignInRequest authSignInRequest) {
         return ResponseEntity.ok(authService.signIn(authSignInRequest));
     }
 
+    @Operation(summary = "accessToken 재발급", description = "accessToken 유효기간 1시간, refreshToken 유효기간 1주")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "재발급 성공"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 refreshToken",
+                            content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 회원에 대해 요청됨",
+                            content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissueToken(@Valid @RequestBody AuthReissueRequest authReissueRequest) {
         return ResponseEntity.ok(authService.reissueToken(authReissueRequest));
