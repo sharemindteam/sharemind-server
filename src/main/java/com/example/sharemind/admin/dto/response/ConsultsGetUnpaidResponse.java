c95c28e7a5281e8950c630e184b45fa8a13a0b13
letskuku <yujung9288@naver.com>
2024-01-13 00:25:07 +0900

    #23 refactor: admin 스웨거 설명 추가

--- src/main/java/com/example/sharemind/admin/dto/response/ConsultsGetUnpaidResponse.java
+++ src/main/java/com/example/sharemind/admin/dto/response/ConsultsGetUnpaidResponse.java
@@ -1,6 +1,7 @@
 package com.example.sharemind.admin.dto.response;
 
 import com.example.sharemind.consult.domain.Consult;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +12,22 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConsultsGetUnpaidResponse {
 
+    @Schema(description = "상담 아이디")
     private final Long consultId;
+
+    @Schema(description = "구매자 닉네임")
     private final String customerName;
+
+    @Schema(description = "판매자 닉네임")
     private final String counselorName;
+
+    @Schema(description = "상담 유형", example = "편지")
     private final String consultType;
+
+    @Schema(description = "상담료")
     private final Long cost;
+
+    @Schema(description = "상담 신청 일시")
     private final LocalDateTime createdAt;
 
     public static ConsultsGetUnpaidResponse of(Consult consult) {
