c5e7499180a48f784abbfd75f33da4e1e08f83e9
aeyongdodam <kikititi0510@gmail.com>
2024-01-12 15:31:07 +0900

    #14 feat: chatInfoGetResponse dto 생성

---
+++ src/main/java/com/example/sharemind/chat/dto/response/ChatInfoGetResponse.java
@@ -0,0 +1,31 @@
+package com.example.sharemind.chat.dto.response;
+
+import com.example.sharemind.chat.domain.Chat;
+import com.example.sharemind.chatMessage.domain.ChatMessage;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChatInfoGetResponse {
+
+    private final Long chatId;
+    private final String opponentNickname;
+    private final String chatStatus;
+    private final int unreadMessageCount;
+    private final String latestMessageContent;
+    private final LocalDateTime latestMessageUpdatedAt;
+    private final Boolean latestMessageIsCustomer;
+
+    public static ChatInfoGetResponse of(String nickname, int unreadMessageCount, Chat chat, ChatMessage chatMessage) {
+        if (chatMessage == null) {
+            return new ChatInfoGetResponse(chat.getChatId(), nickname, chat.getChatStatus()
+                    .getDisplayName(), unreadMessageCount, null, null, null);
+        }
+        return new ChatInfoGetResponse(chat.getChatId(), nickname, chat.getChatStatus()
+                .getDisplayName(), unreadMessageCount, chatMessage.getContent(), chatMessage.getUpdatedAt(),
+                chatMessage.getIsCustomer());
+    }
+}
