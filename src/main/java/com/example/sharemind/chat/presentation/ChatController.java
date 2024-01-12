6a6a500e81d3580e38c1c7861770b4ea8221dc36
aeyongdodam <kikititi0510@gmail.com>
2024-01-12 15:31:23 +0900

    #14 feat: chatInfo api 생성

--- src/main/java/com/example/sharemind/chat/presentation/ChatController.java
+++ src/main/java/com/example/sharemind/chat/presentation/ChatController.java
@@ -1,10 +1,13 @@
 package com.example.sharemind.chat.presentation;
 
 import com.example.sharemind.chat.application.ChatService;
+import com.example.sharemind.chat.dto.response.ChatInfoGetResponse;
+import com.example.sharemind.global.jwt.CustomUserDetails;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +19,12 @@ public class ChatController {
 
     private final ChatService chatService;
 
-    @GetMapping("/channels") //todo: 채팅 데모용을 위한 api 삭제해야함
-    public ResponseEntity<List<Long>> getChannelList(@RequestParam Long userId, @RequestParam Boolean isCustomer) {
-        return ResponseEntity.ok(chatService.getChatsByUserId(userId, isCustomer));
+    @GetMapping("/chat")
+    public ResponseEntity<List<ChatInfoGetResponse>> getChatList(@RequestParam Boolean isCustomer,
+                                                                 @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        List<ChatInfoGetResponse> chatInfoGetResponses = chatService.getChatInfoByCustomerId(
+                customUserDetails.getCustomer().getCustomerId(), isCustomer);
+        return ResponseEntity.ok(chatInfoGetResponses);
+        //todo: 메세지 읽지 않은 순, 완료/취소된 상담 포함한 것도 구현해야함ㅜㅜ
     }
 }
