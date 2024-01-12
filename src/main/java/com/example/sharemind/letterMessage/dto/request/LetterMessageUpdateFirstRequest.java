d547089d968cb9120defcdd56e05e83d0c3ef126
letskuku <yujung9288@naver.com>
2024-01-13 01:22:02 +0900

    #23 refactor: letterMessage 스웨거 설명 추가

--- src/main/java/com/example/sharemind/letterMessage/dto/request/LetterMessageUpdateFirstRequest.java
+++ src/main/java/com/example/sharemind/letterMessage/dto/request/LetterMessageUpdateFirstRequest.java
@@ -1,5 +1,6 @@
 package com.example.sharemind.letterMessage.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -7,15 +8,19 @@ import lombok.Getter;
 @Getter
 public class LetterMessageUpdateFirstRequest {
 
+    @Schema(description = "메시지 아이디", example = "1")
     @NotNull(message = "메시지 id는 공백일 수 없습니다.")
     private Long messageId;
 
+    @Schema(description = "선택한 상담 카테고리", example = "이별/재회")
     @NotBlank(message = "상담 카테고리는 공백일 수 없습니다.")
     private String consultCategory;
 
+    @Schema(description = "메시지 내용", example = "안녕하세요 어쩌구저쩌구~")
     @NotBlank(message = "메시지 내용은 공백일 수 없습니다.")
     private String content;
 
+    @Schema(description = "임시저장/최종 제출 여부", example = "true")
     @NotNull(message = "작성 완료 여부는 공백일 수 없습니다.")
     private Boolean isCompleted;
 }
--- src/main/java/com/example/sharemind/letterMessage/dto/response/LetterMessageGetIsSavedResponse.java
+++ src/main/java/com/example/sharemind/letterMessage/dto/response/LetterMessageGetIsSavedResponse.java
@@ -2,6 +2,7 @@ package com.example.sharemind.letterMessage.dto.response;
 
 import com.example.sharemind.letterMessage.domain.LetterMessage;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,10 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class LetterMessageGetIsSavedResponse {
 
+    @Schema(description = "임시저장 메시지 존재하면 true, 아니면 false", example = "true")
     private final Boolean isSaved;
+
+    @Schema(description = "마지막 수정일시, isSaved false면 null", example = "2023년 12월 25일 오후 12시 34분")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 a HH시 mm분")
     private final LocalDateTime updatedAt;
 
