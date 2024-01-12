947ad6ea569b98c7a5a464415b271b7af36288b0
letskuku <yujung9288@naver.com>
2024-01-13 00:47:00 +0900

    #23 refactor: letter 스웨거 설명 추가

--- src/main/java/com/example/sharemind/letter/dto/response/LetterGetCounselorCategoriesResponse.java
+++ src/main/java/com/example/sharemind/letter/dto/response/LetterGetCounselorCategoriesResponse.java
@@ -2,6 +2,7 @@ package com.example.sharemind.letter.dto.response;
 
 import com.example.sharemind.global.content.ConsultCategory;
 import com.example.sharemind.letter.domain.Letter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class LetterGetCounselorCategoriesResponse {
 
+    @Schema(description = "상담 카테고리", example = "이별/재회, 권태기, 기타")
     private final List<String> categories;
 
     public static LetterGetCounselorCategoriesResponse of(Letter letter) {
