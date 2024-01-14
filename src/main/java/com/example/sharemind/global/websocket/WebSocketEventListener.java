3e96c92a30c4e5d2900d4152197f74dc76eb9bf0
aeyongdodam <kikititi0510@gmail.com>
2024-01-15 03:14:14 +0900

    #14 feat: websocket connect/disconnect 시 eventListener 생성

---
+++ src/main/java/com/example/sharemind/global/websocket/WebSocketEventListener.java
@@ -0,0 +1,31 @@
+package com.example.sharemind.global.websocket;
+
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageSendingOperations;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        log.info("new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String nickname = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("userNickname");
+        log.info("{} disconnected", nickname);
+    }
+}
