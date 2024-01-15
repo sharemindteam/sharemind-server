be9625fc5f4bed0cbf5bfa4094ce6c22590a45c4
aeyongdodam <kikititi0510@gmail.com>
2024-01-16 01:09:08 +0900

    #31 feat: EmailController 스웨거 적용

--- src/main/java/com/example/sharemind/email/presentation/EmailController.java
+++ src/main/java/com/example/sharemind/email/presentation/EmailController.java
@@ -4,6 +4,10 @@ import com.example.sharemind.customer.application.CustomerService;
 import com.example.sharemind.email.application.EmailService;
 import com.example.sharemind.email.dto.request.EmailPostCodeRequest;
 import com.example.sharemind.email.dto.request.EmailPostRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Email Controller", description = "이메일 컨트롤러")
 @RestController
 @RequestMapping("/api/v1/emails")
 @RequiredArgsConstructor
@@ -20,6 +25,11 @@ public class EmailController {
     private final EmailService emailService;
     private final CustomerService customerService;
 
+    @Operation(summary = "이메일 인증코드 발송", description = "회원가입 시 이메일 인증코드를 발송")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "해당 이메일로 인증 코드 발송 성공"),
+            @ApiResponse(responseCode = "400", description = "1. 이미 회원으로 등록된 이메일\n 2. 유효기간이 만료되지 않은 코드가 남아있을 경우\n3. 올바르지 않은 이메일 형식")
+    })
     @PostMapping
     public ResponseEntity<Void> sendVerificationCode(@Valid @RequestBody EmailPostRequest emailPostRequest) {
         customerService.checkDuplicateEmail(emailPostRequest.getEmail());
@@ -28,6 +38,11 @@ public class EmailController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "회원가입 이메일 인증코드 검증", description = "회원가입 이메일 인증코드가 일치하는지 검증")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "검증 성공"),
+            @ApiResponse(responseCode = "400", description = "1. 잘못된 인증코드\n2. 올바르지 않은 이메일 형식")
+    })
     @PostMapping("/code")
     public ResponseEntity<Void> verifyCode(@Valid @RequestBody EmailPostCodeRequest emailPostCodeRequest) {
         emailService.verifyCode(emailPostCodeRequest.getEmail(), emailPostCodeRequest.getCode());
