0caf380c59070567b90d9f51d97e13dcf4d2aafe
aeyongdodam <kikititi0510@gmail.com>
2024-01-16 01:08:55 +0900

    #31 fix: 안쓰는 EmailErrorCode 삭제

--- src/main/java/com/example/sharemind/email/exception/EmailErrorCode.java
+++ src/main/java/com/example/sharemind/email/exception/EmailErrorCode.java
@@ -7,8 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum EmailErrorCode {
 
     CODE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "아직 만료되지 않은 코드가 남아있습니다."),
-    CODE_MISMATCH(HttpStatus.BAD_REQUEST, "잘못된 인증 코드입니다."),
-    INVALID_EMAIL(HttpStatus.FORBIDDEN, "잘못된 이메일 형식입니다.");
+    CODE_MISMATCH(HttpStatus.BAD_REQUEST, "잘못된 인증 코드입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
