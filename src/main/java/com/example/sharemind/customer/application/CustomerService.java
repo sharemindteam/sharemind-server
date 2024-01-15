131b42eb24c3f9a02fd5cc8ec9c05e2ee0cfe2e4
aeyongdodam <kikititi0510@gmail.com>
2024-01-16 00:31:33 +0900

    #31 feat: email 중복 검사 함수 작성

--- src/main/java/com/example/sharemind/customer/application/CustomerService.java
+++ src/main/java/com/example/sharemind/customer/application/CustomerService.java
@@ -4,4 +4,6 @@ import com.example.sharemind.customer.domain.Customer;
 
 public interface CustomerService {
     Customer getCustomerByCustomerId(Long customerId);
+
+    void checkDuplicateEmail(String email);
 }
