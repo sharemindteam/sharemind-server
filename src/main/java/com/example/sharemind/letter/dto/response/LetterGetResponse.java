5b7c3e45a85a77aae8c4ebc3c6ab709f07a7bafc
letskuku <yujung9288@naver.com>
2024-01-18 02:17:09 +0900

    #45 feat: 계층 간 편지 정보 이동 위한 dto 구현

--- src/main/java/com/example/sharemind/letter/dto/response/LetterGetResponse.java
+++ src/main/java/com/example/sharemind/letter/dto/response/LetterGetResponse.java
@@ -1,9 +1,15 @@
 package com.example.sharemind.letter.dto.response;
 
 import com.example.sharemind.letter.domain.Letter;
+import com.example.sharemind.letterMessage.domain.LetterMessage;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,7 +21,11 @@ public class LetterGetResponse {
 
     private final String opponentName;
 
-    public static LetterGetResponse of(Letter letter, Boolean isCustomer) {
+    private final String updatedAt;
+
+    private final String recentContent;
+
+    public static LetterGetResponse of(Letter letter, LetterMessage recentMessage, Boolean isCustomer) {
         String letterStatus;
         String opponentName;
         if (isCustomer) {
@@ -26,6 +36,22 @@ public class LetterGetResponse {
             opponentName = letter.getConsult().getCustomer().getNickname();
         }
 
-        return new LetterGetResponse(letter.getLetterId(), letterStatus, opponentName);
+        if (recentMessage == null) {
+            return new LetterGetResponse(letter.getLetterId(), letterStatus, opponentName, null, null);
+        }
+
+        return new LetterGetResponse(letter.getLetterId(), letterStatus, opponentName, getUpdatedAt(recentMessage.getUpdatedAt()), recentMessage.getContent());
+    }
+
+    private static String getUpdatedAt(LocalDateTime updatedAt) {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (ChronoUnit.DAYS.between(now, updatedAt) > 0) {
+            return updatedAt.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+        } else if (ChronoUnit.HOURS.between(now, updatedAt) > 0) {
+            return ChronoUnit.HOURS.between(now, updatedAt) + "시간 전";
+        } else {
+            return ChronoUnit.MINUTES.between(now, updatedAt) + "분 전";
+        }
     }
 }
