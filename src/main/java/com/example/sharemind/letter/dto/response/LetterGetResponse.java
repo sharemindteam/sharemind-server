8f0bcfbd4763bdbbc3df6a3c75b817af8c39af0a
letskuku <yujung9288@naver.com>
2024-01-18 21:01:31 +0900

    #45 feat: 편지 마지막 업데이트 일시 반환 형식 수정

--- src/main/java/com/example/sharemind/letter/dto/response/LetterGetResponse.java
+++ src/main/java/com/example/sharemind/letter/dto/response/LetterGetResponse.java
@@ -52,12 +52,20 @@ public class LetterGetResponse {
     private static String getUpdatedAt(LocalDateTime updatedAt) {
         LocalDateTime now = LocalDateTime.now();
 
-        if (ChronoUnit.DAYS.between(updatedAt, now) > 0) {
+        if (ChronoUnit.YEARS.between(updatedAt, now) > 0) {
             return updatedAt.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+        } else if (ChronoUnit.DAYS.between(updatedAt, now) > 0) {
+            if (ChronoUnit.DAYS.between(updatedAt, now) == 1) {
+                return "어제";
+            } else {
+                return updatedAt.format(DateTimeFormatter.ofPattern("MM.dd"));
+            }
         } else if (ChronoUnit.HOURS.between(updatedAt, now) > 0) {
-            return ChronoUnit.HOURS.between(updatedAt, now) + "시간 전";
-        } else {
+            return updatedAt.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+        } else if (ChronoUnit.MINUTES.between(updatedAt, now) > 0) {
             return ChronoUnit.MINUTES.between(updatedAt, now) + "분 전";
+        } else {
+            return "방금";
         }
     }
 }
