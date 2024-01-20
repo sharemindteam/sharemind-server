4106c1c7f5fbfe8f52b0f7d6404f03d50e2f3b25
letskuku <yujung9288@naver.com>
2024-01-21 01:48:56 +0900

    #65 feat: 리뷰 저장하는 비즈니스 로직 구현

--- src/main/java/com/example/sharemind/review/domain/Review.java
+++ src/main/java/com/example/sharemind/review/domain/Review.java
@@ -2,7 +2,12 @@ package com.example.sharemind.review.domain;
 
 import com.example.sharemind.consult.domain.Consult;
 import com.example.sharemind.global.common.BaseEntity;
+import com.example.sharemind.review.exception.ReviewErrorCode;
+import com.example.sharemind.review.exception.ReviewException;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,8 +20,11 @@ public class Review extends BaseEntity {
     @Column(name = "review_id")
     private Long reviewId;
 
+    @Min(value = 1, message = "평점은 최소 1입니다.")
+    @Max(value = 5, message = "평점은 최대 5입니다.")
     private Integer rating;
 
+    @Size(max = 500, message = "리뷰 내용은 최대 500자입니다.")
     @Column(columnDefinition = "TEXT")
     private String comment;
 
@@ -30,5 +38,19 @@ public class Review extends BaseEntity {
     public Review(Consult consult) {
         this.consult = consult;
         this.isCompleted = false;
+    }
+
+    public void updateReview(Integer rating, String comment) {
+        validateReview();
+
+        this.rating = rating;
+        this.comment = comment;
+        this.isCompleted = true;
+    }
+
+    private void validateReview() {
+        if (this.isCompleted) {
+            throw new ReviewException(ReviewErrorCode.REVIEW_ALREADY_COMPLETED);
+        }
     }
 }
