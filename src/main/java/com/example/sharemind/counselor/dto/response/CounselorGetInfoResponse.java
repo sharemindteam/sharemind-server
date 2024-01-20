3c929adb4dceec6bfe46b55603dd6dbc72ecae67
letskuku <yujung9288@naver.com>
2024-01-20 17:05:46 +0900

    #59 feat: 계층 간 상담사 내 정보 이동 위한 dto 구현

---
+++ src/main/java/com/example/sharemind/counselor/dto/response/CounselorGetInfoResponse.java
@@ -0,0 +1,35 @@
+package com.example.sharemind.counselor.dto.response;
+
+import com.example.sharemind.counselor.content.ProfileStatus;
+import com.example.sharemind.counselor.domain.Counselor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class CounselorGetInfoResponse {
+
+    @Schema(description = "닉네임")
+    private final String nickname;
+
+    @Schema(description = "레벨")
+    private final Integer level;
+
+    @Schema(description = "교육 수료 여부")
+    private final Boolean isEducated;
+
+    @Schema(description = "프로필 정보 상태", example = "프로필 정보 없음")
+    private final String profileStatus;
+
+    public static CounselorGetInfoResponse of() {
+        return new CounselorGetInfoResponse("연애상담마스터", 0, false,
+                ProfileStatus.NO_PROFILE.name());
+    }
+
+    public static CounselorGetInfoResponse of(Counselor counselor) {
+        return new CounselorGetInfoResponse(counselor.getNickname(), counselor.getLevel(), counselor.getIsEducated(),
+                counselor.getProfileStatus().name());
+    }
+}
