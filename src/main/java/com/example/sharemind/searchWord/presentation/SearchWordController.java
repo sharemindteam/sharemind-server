2f54a56aa90b31d34f568ea2ce60ea885bc64a26
aeyongdodam <kikititi0510@gmail.com>
2024-01-20 16:25:14 +0900

    #58 feat: 검색어 저장 및 반환 컨트롤러 구현

--- src/main/java/com/example/sharemind/searchWord/presentation/SearchWordController.java
+++ src/main/java/com/example/sharemind/searchWord/presentation/SearchWordController.java
@@ -1,12 +1,31 @@
 package com.example.sharemind.searchWord.presentation;
 
+import com.example.sharemind.counselor.dto.response.CounselorGetResponse;
+import com.example.sharemind.global.jwt.CustomUserDetails;
+import com.example.sharemind.searchWord.application.SearchWordService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "SearchWord Controller", description = "검색어 관련 컨트롤러")
 @RestController
 @RequestMapping("/api/v1/searchWords")
 @RequiredArgsConstructor
 public class SearchWordController {
-    
+
+    private final SearchWordService searchWordService;
+
+    @GetMapping()
+    public ResponseEntity<List<CounselorGetResponse>> getCounselorCategories(@RequestParam String word,
+                                                                             @RequestParam int index,
+                                                                             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(
+                searchWordService.getSearchWordAndReturnResults(customUserDetails.getCustomer(), word, index));
+    }
 }
