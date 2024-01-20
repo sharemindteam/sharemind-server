c6b2a7378e58f62ee910bc3c26464ccf68eddd2f
aeyongdodam <kikititi0510@gmail.com>
2024-01-20 16:46:55 +0900

    #58 feat: 검색어 삭제 api 생성

--- src/main/java/com/example/sharemind/searchWord/presentation/SearchWordController.java
+++ src/main/java/com/example/sharemind/searchWord/presentation/SearchWordController.java
@@ -3,14 +3,18 @@ package com.example.sharemind.searchWord.presentation;
 import com.example.sharemind.counselor.dto.response.CounselorGetResponse;
 import com.example.sharemind.global.jwt.CustomUserDetails;
 import com.example.sharemind.searchWord.application.SearchWordService;
+import com.example.sharemind.searchWord.dto.request.SearchWordDeleteRequest;
+import com.example.sharemind.searchWord.dto.request.SearchWordFindRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "SearchWord Controller", description = "검색어 관련 컨트롤러")
@@ -27,11 +31,21 @@ public class SearchWordController {
         return ResponseEntity.ok(searchWordService.getRecentSearchWordsByCustomer(customUserDetails.getCustomer()));
     }
 
-    @GetMapping("results")
-    public ResponseEntity<List<CounselorGetResponse>> getCounselorCategories(@RequestParam String word,
-                                                                             @RequestParam int index,
-                                                                             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    @PatchMapping("results")
+    public ResponseEntity<List<CounselorGetResponse>> getSearchWordResults(
+            @Valid @RequestBody SearchWordFindRequest searchWordFindRequest,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return ResponseEntity.ok(
-                searchWordService.getSearchWordAndReturnResults(customUserDetails.getCustomer(), word, index));
+                searchWordService.getSearchWordAndReturnResults(customUserDetails.getCustomer(),
+                        searchWordFindRequest));
+    }
+
+    @PatchMapping()
+    public ResponseEntity<Void> deleteCustomerSearchWord(
+            @Valid @RequestBody SearchWordDeleteRequest searchWordDeleteRequest,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        searchWordService.removeSearchWordByCustomer(customUserDetails.getCustomer(), searchWordDeleteRequest);
+
+        return ResponseEntity.ok().build();
     }
 }
