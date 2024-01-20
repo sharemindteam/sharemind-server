259ca1f48dd80da5299971ed6cdbb3d21ff85315
letskuku <yujung9288@naver.com>
2024-01-21 02:56:03 +0900

    #65 feat: 상담사 내 정보 조회 시 상담 스타일 필드 추가

--- src/main/java/com/example/sharemind/counselor/dto/response/CounselorGetInfoResponse.java
+++ src/main/java/com/example/sharemind/counselor/dto/response/CounselorGetInfoResponse.java
@@ -16,6 +16,9 @@ public class CounselorGetInfoResponse {
 
     @Schema(description = "레벨")
     private final Integer level;
+    
+    @Schema(description = "상담 스타일")
+    private final String consultStyle;
 
     @Schema(description = "교육 수료 여부")
     private final Boolean isEducated;
@@ -24,12 +27,17 @@ public class CounselorGetInfoResponse {
     private final String profileStatus;
 
     public static CounselorGetInfoResponse of() {
-        return new CounselorGetInfoResponse("연애상담마스터", 0, false,
+        return new CounselorGetInfoResponse("연애상담마스터", 0, null, false,
                 ProfileStatus.NO_PROFILE.name());
     }
 
     public static CounselorGetInfoResponse of(Counselor counselor) {
-        return new CounselorGetInfoResponse(counselor.getNickname(), counselor.getLevel(), counselor.getIsEducated(),
+        String consultStyle = null;
+        if (counselor.getConsultStyle() != null) {
+            consultStyle = counselor.getConsultStyle().getDisplayName();
+        }
+        
+        return new CounselorGetInfoResponse(counselor.getNickname(), counselor.getLevel(), consultStyle, counselor.getIsEducated(),
                 counselor.getProfileStatus().name());
     }
 }
