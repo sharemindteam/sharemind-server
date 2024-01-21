0a7bcf52a990a8d45e801cb53646a47c995a92c5
letskuku <yujung9288@naver.com>
2024-01-21 23:03:28 +0900

    #72 feat: 상담사 프로필 후기탭, 홈화면에 필요한 리뷰 리스트 조회 api 구현

--- src/main/java/com/example/sharemind/review/presentation/ReviewController.java
+++ src/main/java/com/example/sharemind/review/presentation/ReviewController.java
@@ -5,6 +5,7 @@ import com.example.sharemind.global.jwt.CustomUserDetails;
 import com.example.sharemind.review.application.ReviewService;
 import com.example.sharemind.review.dto.request.ReviewSaveRequest;
 import com.example.sharemind.review.dto.response.ReviewGetResponse;
+import com.example.sharemind.review.dto.response.ReviewGetShortResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -26,6 +27,9 @@ import java.util.List;
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
+    private static final int COUNSELOR_HOME_PAGE_NUMBER = 0;
+    private static final int COUNSELOR_HOME_PAGE_SIZE = 2;
+    private static final int COUNSELOR_PROFILE_PAGE_SIZE = 3;
 
     private final ReviewService reviewService;
 
@@ -55,7 +59,7 @@ public class ReviewController {
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "구매자 리뷰 조회", description = "- 구매자 리뷰 관리 탭에서 리뷰 작성/남긴 리뷰에 해당하는 리뷰 리스트 조회\n " +
+    @Operation(summary = "구매자 리뷰 조회", description = "- 구매자 페이지 리뷰 관리 탭에서 리뷰 작성/남긴 리뷰에 해당하는 리뷰 리스트 조회\n " +
             "- 주소 형식: /api/v1/reviews/customers?isCompleted=true&pageNumber=0")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "조회 성공(필요하지 않은 값은 null로 반환)"),
@@ -77,7 +81,7 @@ public class ReviewController {
                 customUserDetails.getCustomer().getCustomerId()));
     }
 
-    @Operation(summary = "상담사 리뷰 조회", description = "- 상담사 받은 리뷰 탭에 해당하는 리뷰 리스트 조회\n " +
+    @Operation(summary = "상담사 리뷰 조회", description = "- 상담사 페이지 받은 리뷰 탭에 해당하는 리뷰 리스트 조회\n " +
             "- 주소 형식: /api/v1/reviews/counselors?pageNumber=0")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "조회 성공(필요하지 않은 값은 null로 반환)"),
@@ -99,5 +103,44 @@ public class ReviewController {
                                                                          @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return ResponseEntity.ok(reviewService.getReviewsByCounselor(pageNumber,
                 customUserDetails.getCustomer().getCustomerId()));
+    }
+
+    @Operation(summary = "상담사 홈화면 받은 리뷰 조회", description = "상담사 페이지 홈화면에 필요한 최신 리뷰 2개 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "403", description = "아직 프로필 심사가 완료되지 않은 상담사",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 상담사",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
+    @GetMapping("/counselors/home")
+    public ResponseEntity<List<ReviewGetShortResponse>> getReviewsForCounselorHome(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(reviewService.getShortReviews(COUNSELOR_HOME_PAGE_NUMBER, COUNSELOR_HOME_PAGE_SIZE,
+                customUserDetails.getCustomer().getCounselor().getCounselorId()));
+    }
+
+    @Operation(summary = "상담사 프로필 후기 탭 리뷰 조회", description = "- 구매자 페이지 상담사 프로필 후기 탭에 해당하는 리뷰 리스트 조회\n " +
+            "- 주소 형식: /api/v1/reviews/{counselorId}?pageNumber=0")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 상담사",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
+    @Parameters({
+            @Parameter(name = "counselorId", description = "상담사 아이디"),
+            @Parameter(name = "pageNumber", description = "조회 결과는 3개씩 반환하며, 페이지 번호로 구분" +
+                    "(ex. pageNumber 0: 가장 앞 3개 반환, pageNumber 1: 그다음 3개 반환)")
+    })
+    @GetMapping("/{counselorId}")
+    public ResponseEntity<List<ReviewGetShortResponse>> getReviewsForCounselorProfile(@PathVariable Long counselorId,
+                                                                                      @RequestParam int pageNumber) {
+        return ResponseEntity.ok(reviewService.getShortReviews(pageNumber, COUNSELOR_PROFILE_PAGE_SIZE, counselorId));
     }
 }
