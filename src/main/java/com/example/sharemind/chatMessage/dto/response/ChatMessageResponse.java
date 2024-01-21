ffa56e9f3b49b014b92bdaffd13803ebe0d6553d
aeyongdodam <kikititi0510@gmail.com>
2024-01-21 18:18:22 +0900

    #70 fix: chat response 시간 리턴 타입 수정

--- src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageResponse.java
+++ src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageResponse.java
@@ -1,12 +1,10 @@
 package com.example.sharemind.chatMessage.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.example.sharemind.global.utils.TimeUtil;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cglib.core.Local;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -16,12 +14,11 @@ public class ChatMessageResponse {
 
     private final String content;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 a HH시 mm분")
-    private final LocalDateTime sendTime;
+    private final String sendTime;
 
     private final Boolean isCustomer;
 
     public static ChatMessageResponse of(String senderName, String content, Boolean isCustomer) {
-        return new ChatMessageResponse(senderName, content, LocalDateTime.now(), isCustomer);
+        return new ChatMessageResponse(senderName, content, TimeUtil.getUpdatedAt(LocalDateTime.now()), isCustomer);
     }
 }
