4d79ef382c59922b48f5116ce184385ebf2c96f4
aeyongdodam <kikititi0510@gmail.com>
2024-01-23 01:51:19 +0900

    #70 fix: 채팅 시간 형식 수정

--- src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageGetResponse.java
+++ src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageGetResponse.java
@@ -3,7 +3,8 @@ package com.example.sharemind.chatMessage.dto.response;
 import com.example.sharemind.chat.domain.Chat;
 import com.example.sharemind.chatMessage.domain.ChatMessage;
 import com.example.sharemind.consult.domain.Consult;
-import com.example.sharemind.global.utils.TimeUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +21,15 @@ public class ChatMessageGetResponse {
 
     private final String content;
 
-    private final String sendTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 a HH시 mm분")
+    private final LocalDateTime sendTime;
 
     private final Boolean isCustomer;
 
     public static ChatMessageGetResponse of(Chat chat, ChatMessage chatMessage) {
         Consult consult = chat.getConsult();
         return new ChatMessageGetResponse(consult.getCustomer().getNickname(), consult.getCounselor().getNickname(),
-                chatMessage.getMessageId(), chatMessage.getContent(), TimeUtil.getUpdatedAt(chatMessage.getUpdatedAt()),
+                chatMessage.getMessageId(), chatMessage.getContent(), chatMessage.getUpdatedAt(),
                 chatMessage.getIsCustomer());
     }
 }
--- src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageWebSocketResponse.java
+++ src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageWebSocketResponse.java
@@ -1,6 +1,6 @@
 package com.example.sharemind.chatMessage.dto.response;
 
-import com.example.sharemind.global.utils.TimeUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,12 +14,13 @@ public class ChatMessageWebSocketResponse {
 
     private final String content;
 
-    private final String sendTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 a HH시 mm분")
+    private final LocalDateTime sendTime;
 
     private final Boolean isCustomer;
 
     public static ChatMessageWebSocketResponse of(String senderName, String content, Boolean isCustomer) {
-        return new ChatMessageWebSocketResponse(senderName, content, TimeUtil.getUpdatedAt(LocalDateTime.now()),
+        return new ChatMessageWebSocketResponse(senderName, content, LocalDateTime.now(),
                 isCustomer);
     }
 }
