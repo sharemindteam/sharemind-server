4233438ef931930371b9e9a884c1bccdcd9b56ad
aeyongdodam <kikititi0510@gmail.com>
2024-01-24 02:30:31 +0900

    #70 feat: chatMessage dto에 대한 설명 추가

--- src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageGetResponse.java
+++ src/main/java/com/example/sharemind/chatMessage/dto/response/ChatMessageGetResponse.java
@@ -4,6 +4,7 @@ import com.example.sharemind.chat.domain.Chat;
 import com.example.sharemind.chatMessage.domain.ChatMessage;
 import com.example.sharemind.consult.domain.Consult;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,17 +14,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChatMessageGetResponse {
 
+    @Schema(description = "구매자 닉네임")
     private final String customerNickname;
 
+    @Schema(description = "상담사 닉네임")
     private final String counselorNickname;
 
+    @Schema(description = "메세지 id")
     private final Long messageId;
 
+    @Schema(description = "메세지 내용")
     private final String content;
 
+    @Schema(description = "메세지 전송 시간")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 a HH시 mm분")
     private final LocalDateTime sendTime;
 
+    @Schema(description = "구매자이면 true")
     private final Boolean isCustomer;
 
     public static ChatMessageGetResponse of(Chat chat, ChatMessage chatMessage) {
