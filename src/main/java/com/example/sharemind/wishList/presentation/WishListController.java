462c64f0089565b9d908e8a8ac4fbc80ee04e2d7
aeyongdodam <kikititi0510@gmail.com>
2024-01-25 15:00:33 +0900

    #82 feat: 찜하기 컨트롤러 작성

--- src/main/java/com/example/sharemind/wishList/presentation/WishListController.java
+++ src/main/java/com/example/sharemind/wishList/presentation/WishListController.java
@@ -1,8 +1,15 @@
 package com.example.sharemind.wishList.presentation;
 
+import com.example.sharemind.global.jwt.CustomUserDetails;
+import com.example.sharemind.wishList.application.WishListService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "WishList Controller", description = "찜하기 컨트롤러")
@@ -11,4 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class WishListController {
 
+    private WishListService wishListService;
+
+    @PatchMapping()
+    public ResponseEntity<Void> addWishList(@RequestParam Long counselorId,
+                                            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        wishListService.addWishListByCustomer(customUserDetails.getCustomer().getCustomerId(), counselorId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<Void> removeWishList(@RequestParam Long counselorId,
+                                               @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        wishListService.removeWishListByCustomer(customUserDetails.getCustomer().getCustomerId(), counselorId);
+        return ResponseEntity.ok().build();
+    }
 }
