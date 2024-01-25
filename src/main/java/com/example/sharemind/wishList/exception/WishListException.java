2d121c6bc0709cdcf6839e3fe10361c9bc151979
aeyongdodam <kikititi0510@gmail.com>
2024-01-25 14:54:40 +0900

    #82 fix: 안쓰는 함수 삭제

--- src/main/java/com/example/sharemind/wishList/exception/WishListException.java
+++ src/main/java/com/example/sharemind/wishList/exception/WishListException.java
@@ -7,11 +7,6 @@ public class WishListException extends RuntimeException {
 
     private final WishListErrorCode errorCode;
 
-    public WishListException(WishListErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
-
     public WishListException(WishListErrorCode errorCode, String message) {
         super(errorCode.getMessage() + " : " + message);
         this.errorCode = errorCode;
