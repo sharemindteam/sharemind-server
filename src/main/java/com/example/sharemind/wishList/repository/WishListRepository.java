f09bce06acbf055de654c5717b257f3ad8f038b8
aeyongdodam <kikititi0510@gmail.com>
2024-01-25 19:51:06 +0900

    #82 feat: 찜하기 레포지토리단 생성

--- src/main/java/com/example/sharemind/wishList/repository/WishListRepository.java
+++ src/main/java/com/example/sharemind/wishList/repository/WishListRepository.java
@@ -4,6 +4,8 @@ import com.example.sharemind.counselor.domain.Counselor;
 import com.example.sharemind.customer.domain.Customer;
 import com.example.sharemind.wishList.domain.WishList;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,8 @@ public interface WishListRepository extends JpaRepository<WishList, Long> {
     List<WishList> findByCustomerAndIsActivatedIsTrue(Customer customer);
 
     WishList findByCustomerAndCounselor(Customer customer, Counselor counselor);
+
+    Page<WishList> findByCustomerAndWishlistIdLessThanAndIsActivatedIsTrueOrderByUpdatedAtDesc(Customer customer,
+                                                                                               Long wishlistId,
+                                                                                               Pageable pageable);
 }
