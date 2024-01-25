827c2d49953d916aa945619fade8b366e4fba7d8
aeyongdodam <kikititi0510@gmail.com>
2024-01-25 19:50:04 +0900

    #82 feat: 찜하기 리스트 컨트롤러 생성

--- src/main/java/com/example/sharemind/wishList/presentation/WishListController.java
+++ src/main/java/com/example/sharemind/wishList/presentation/WishListController.java
@@ -1,16 +1,15 @@
 package com.example.sharemind.wishList.presentation;
 
+import com.example.sharemind.counselor.application.CounselorService;
+import com.example.sharemind.counselor.dto.response.CounselorGetWishListResponse;
 import com.example.sharemind.global.jwt.CustomUserDetails;
 import com.example.sharemind.wishList.application.WishListService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "WishList Controller", description = "찜하기 컨트롤러")
 @RestController
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WishListController {
 
     private final WishListService wishListService;
+    private final CounselorService counselorService;
 
     @PatchMapping()
     public ResponseEntity<Void> addWishList(@RequestParam Long counselorId,
@@ -32,5 +32,13 @@ public class WishListController {
                                                @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         wishListService.removeWishListByCustomer(customUserDetails.getCustomer().getCustomerId(), counselorId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<CounselorGetWishListResponse>> getWishList(@RequestParam Long wishlistId,
+                                                                          @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(counselorService.getCounselorWishListByCustomer(
+                wishlistId, customUserDetails.getCustomer()
+                        .getCustomerId()));
     }
 }
