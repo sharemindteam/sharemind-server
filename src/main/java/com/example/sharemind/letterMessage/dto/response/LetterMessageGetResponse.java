26caec105da7e5a64315aa3c13c5229030e87e4e
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2024-02-01 14:06:40 +0900

    refactor: 시간 형식 알맞게 수정

--- src/main/java/com/example/sharemind/letterMessage/dto/response/LetterMessageGetResponse.java
+++ src/main/java/com/example/sharemind/letterMessage/dto/response/LetterMessageGetResponse.java
@@ -24,7 +24,7 @@ public class LetterMessageGetResponse {
     private final String content;
 
     @Schema(description = "작성 일시, 메시지 존재하지 않으면 null", example = "2024년 1월 4일 오전 9시 45분", type = "string")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 a HH시 mm분")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 a HH시 mm분", locale="ko")
     private final LocalDateTime updatedAt;
 
     @Schema(description = "작성 일시, 메시지 존재하지 않으면 null", example = "01.12")
