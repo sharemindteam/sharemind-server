6372c98951a86498dee85179fc588d1100dfd91e
aeyongdodam <kikititi0510@gmail.com>
2024-02-02 11:16:35 +0900

    #114 feat: AuthFindRequest dto 생성

---
+++ src/main/java/com/example/sharemind/auth/dto/request/AuthFindRequest.java
@@ -0,0 +1,13 @@
+package com.example.sharemind.auth.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class AuthFindRequest {
+
+    @Schema(description = "복구 이메일 주소", example = "abc@gmail.com")
+    @NotBlank(message = "복구 이메일은 공백일 수 없습니다.")
+    private String recoveryEmail;
+}
