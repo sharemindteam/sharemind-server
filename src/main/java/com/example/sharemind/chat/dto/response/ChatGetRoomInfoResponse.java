1a59b6a95d6bfcba14519a008724534e60b798ff
aeyongdodam <kikititi0510@gmail.com>
2024-02-28 03:24:26 +0900

    #130 feat: 구매자 사이트 채팅 정보 dto에 남은 시간 필드 추가

--- src/main/java/com/example/sharemind/chat/dto/response/ChatGetRoomInfoResponse.java
+++ src/main/java/com/example/sharemind/chat/dto/response/ChatGetRoomInfoResponse.java
@@ -1,6 +1,8 @@
 package com.example.sharemind.chat.dto.response;
 
+import com.example.sharemind.chat.content.ChatStatus;
 import com.example.sharemind.chat.domain.Chat;
+import com.example.sharemind.global.utils.TimeUtil;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -16,7 +18,16 @@ public class ChatGetRoomInfoResponse {
     @Schema(description = "채팅 진행 상태")
     private final String status;
 
+    @Schema(description = "채팅 요청 상태일 때, 남은 시간, 채팅 요청 상태 이외에는 null")
+    private final String time;
+
     public static ChatGetRoomInfoResponse of(Chat chat) {
-        return new ChatGetRoomInfoResponse(chat.getConsult().getCustomer().getNickname(), chat.getChatStatus().getDisplayName());
+        if (chat.getChatStatus() == ChatStatus.SEND_REQUEST) {
+            return new ChatGetRoomInfoResponse(chat.getConsult().getCustomer().getNickname(),
+                    chat.getChatStatus().getDisplayName(),
+                    TimeUtil.getChatSendRequestLeftTime(chat.getUpdatedAt()));
+        }
+        return new ChatGetRoomInfoResponse(chat.getConsult().getCustomer().getNickname(),
+                chat.getChatStatus().getDisplayName(), null);
     }
 }
