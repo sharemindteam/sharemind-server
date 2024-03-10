e2ddc2f6c783b344af12e303266cedfb0ec8ce2c
aeyongdodam <kikititi0510@gmail.com>
2024-03-11 03:13:57 +0900

    #137 feat: ChatNotifyEventResponse에 consultStyle 추가

--- src/main/java/com/example/sharemind/chat/dto/response/ChatNotifyEventResponse.java
+++ src/main/java/com/example/sharemind/chat/dto/response/ChatNotifyEventResponse.java
@@ -1,6 +1,7 @@
 package com.example.sharemind.chat.dto.response;
 
 import com.example.sharemind.chat.content.ChatRoomWebsocketStatus;
+import com.example.sharemind.chat.domain.Chat;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -24,7 +25,15 @@ public class ChatNotifyEventResponse {
     @Schema(description = "채팅 이벤트")
     private final ChatRoomWebsocketStatus chatRoomWebsocketStatus;
 
+    @Schema(description = "채팅 스타일")
+    private final String consultStyle;
+
     public static ChatNotifyEventResponse of(Long chatId, ChatRoomWebsocketStatus chatRoomWebsocketStatus) {
-        return new ChatNotifyEventResponse(chatId, LocalDateTime.now(), chatRoomWebsocketStatus);
+        return new ChatNotifyEventResponse(chatId, LocalDateTime.now(), chatRoomWebsocketStatus, null);
+    }
+
+    public static ChatNotifyEventResponse of(Chat chat, ChatRoomWebsocketStatus chatRoomWebsocketStatus) {
+        return new ChatNotifyEventResponse(chat.getChatId(), LocalDateTime.now(), chatRoomWebsocketStatus,
+                chat.getConsult().getCounselor().getConsultStyle().getDisplayName());
     }
 }
