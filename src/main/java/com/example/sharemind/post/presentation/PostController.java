e31bdd7c84a0df2ce9d737f8eba51632a3efd870
letskuku <yujung9288@naver.com>
2024-03-17 00:16:17 +0900

    #139 feat: 일대다 질문 임시저장 내용 존재 여부 조회 api 구현

--- src/main/java/com/example/sharemind/post/presentation/PostController.java
+++ src/main/java/com/example/sharemind/post/presentation/PostController.java
@@ -5,7 +5,10 @@ import com.example.sharemind.global.jwt.CustomUserDetails;
 import com.example.sharemind.post.application.PostService;
 import com.example.sharemind.post.dto.request.PostCreateRequest;
 import com.example.sharemind.post.dto.request.PostUpdateRequest;
+import com.example.sharemind.post.dto.response.PostGetIsSavedResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -16,7 +19,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -72,5 +77,22 @@ public class PostController {
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         postService.updatePost(postUpdateRequest, customUserDetails.getCustomer().getCustomerId());
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "일대다 상담 질문 임시저장 내용 존재 여부 조회",
+            description = "임시저장 내용 존재 여부 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공(임시저장 내용 존재하지 않으면 수정일시는 null로 반환됨)"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 일대다 질문 아이디로 요청됨",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "일대다 질문 아이디")
+    })
+    @GetMapping("/drafts/{postId}")
+    public ResponseEntity<PostGetIsSavedResponse> getIsSaved(@PathVariable Long postId) {
+        return ResponseEntity.ok(postService.getIsSaved(postId));
     }
 }
