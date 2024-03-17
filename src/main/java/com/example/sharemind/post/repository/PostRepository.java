231e866c1117373c1e8aa040081abe954cbea258
aeyongdodam <kikititi0510@gmail.com>
2024-03-18 04:25:10 +0900

    #145 feat: 랜덤 post 50개 리턴 함수 생성

--- src/main/java/com/example/sharemind/post/repository/PostRepository.java
+++ src/main/java/com/example/sharemind/post/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.example.sharemind.post.domain.Post;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +13,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByPostIdAndIsActivatedIsTrue(Long postId);
 
     List<Post> findAllByIsPaidIsFalseAndIsActivatedIsTrue();
+
+    @Query(value = "SELECT post_id FROM post WHERE post_status = 'PROCEEDING' ORDER BY RAND() LIMIT 50", nativeQuery = true)
+    List<Long> findRandomProceedingPostIds();
 }
