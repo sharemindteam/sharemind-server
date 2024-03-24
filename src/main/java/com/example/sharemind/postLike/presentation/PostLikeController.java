4ae2a7bb2efc2c192436f6c7c7d13d3c4f28c8f8
letskuku <yujung9288@naver.com>
2024-03-24 22:02:35 +0900

    #152 feat: 일대다 상담 질문 좋아요 생성 api 구현

---
+++ src/main/java/com/example/sharemind/postLike/presentation/PostLikeController.java
@@ -0,0 +1,53 @@
+package com.example.sharemind.postLike.presentation;
+
+import com.example.sharemind.global.exception.CustomExceptionResponse;
+import com.example.sharemind.global.jwt.CustomUserDetails;
+import com.example.sharemind.postLike.application.PostLikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "PostLike Controller", description = "일대다 상담 질문 좋아요 컨트롤러")
+@RestController
+@RequestMapping("/api/v1/postLikes")
+@RequiredArgsConstructor
+public class PostLikeController {
+
+    private final PostLikeService postLikeService;
+
+    @Operation(summary = "일대다 상담 질문 좋아요 생성", description = "일대다 상담 질문 좋아요 생성")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "생성 성공"),
+            @ApiResponse(responseCode = "400",
+                    description = "이미 좋아요를 누른 질문",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "1. 존재하지 않는 질문\n 2. 존재하지 않는 회원",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "일대다 질문 아이디")
+    })
+    @PostMapping("/{postId}")
+    public ResponseEntity<Void> createPostLike(@PathVariable Long postId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        postLikeService.createPostLike(postId, customUserDetails.getCustomer().getCustomerId());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
