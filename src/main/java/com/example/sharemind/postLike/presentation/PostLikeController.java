c11432aff5c894dc791f9281041b0f978fb00256
letskuku <yujung9288@naver.com>
2024-03-24 22:17:38 +0900

    #152 feat: 일대다 상담 질문 좋아요 취소 api 구현

--- src/main/java/com/example/sharemind/postLike/presentation/PostLikeController.java
+++ src/main/java/com/example/sharemind/postLike/presentation/PostLikeController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +50,26 @@ public class PostLikeController {
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         postLikeService.createPostLike(postId, customUserDetails.getCustomer().getCustomerId());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Operation(summary = "일대다 상담 질문 좋아요 취소", description = "일대다 상담 질문 좋아요 취소")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "취소 성공"),
+            @ApiResponse(responseCode = "404", description = """
+                    1. 존재하지 않는 질문
+                    2. 존재하지 않는 회원
+                    3. 존재하지 않는 좋아요""",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "일대다 질문 아이디")
+    })
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> deletePostLike(@PathVariable Long postId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        postLikeService.deletePostLike(postId, customUserDetails.getCustomer().getCustomerId());
+        return ResponseEntity.ok().build();
     }
 }
