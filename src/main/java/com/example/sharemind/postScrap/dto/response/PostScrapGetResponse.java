a7d263657d59f6d441d99d411bc035b6f97e170e
letskuku <yujung9288@naver.com>
2024-03-28 01:21:57 +0900

    #157 feat: 계층간 일대다 상담 스크랩 정보 이동 위한 dto 구현

---
+++ src/main/java/com/example/sharemind/postScrap/dto/response/PostScrapGetResponse.java
@@ -0,0 +1,80 @@
+package com.example.sharemind.postScrap.dto.response;
+
+import com.example.sharemind.global.utils.TimeUtil;
+import com.example.sharemind.post.domain.Post;
+import com.example.sharemind.postScrap.domain.PostScrap;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostScrapGetResponse {
+
+    @Schema(description = "스크랩 아이디")
+    private final Long postScrapId;
+
+    @Schema(description = "일대다 질문 아이디")
+    private final Long postId;
+
+    @Schema(description = "제목")
+    private final String title;
+
+    @Schema(description = "상담 내용")
+    private final String content;
+
+    @Schema(description = "공개/비공개 여부")
+    private final Boolean isPublic;
+
+    @Schema(description = "조회한 회원의 좋아요 여부")
+    private final Boolean isLiked;
+
+    @Schema(description = "좋아요 수")
+    private final Long totalLike;
+
+    @Schema(description = "조회한 회원의 스크랩 여부")
+    private final Boolean isScrapped;
+
+    @Schema(description = "스크랩 수")
+    private final Long totalScrap;
+
+    @Schema(description = "답변 수")
+    private final Long totalComment;
+
+    @Schema(description = "마지막 업데이트 일시", example = "오전 11:10")
+    private final String updatedAt;
+
+    @Builder
+    public PostScrapGetResponse(Long postScrapId, Long postId, String title, String content,
+            Boolean isPublic, Boolean isLiked, Long totalLike, Boolean isScrapped, Long totalScrap,
+            Long totalComment, String updatedAt) {
+        this.postScrapId = postScrapId;
+        this.postId = postId;
+        this.title = title;
+        this.content = content;
+        this.isPublic = isPublic;
+        this.isLiked = isLiked;
+        this.totalLike = totalLike;
+        this.isScrapped = isScrapped;
+        this.totalScrap = totalScrap;
+        this.totalComment = totalComment;
+        this.updatedAt = updatedAt;
+    }
+
+    public static PostScrapGetResponse of(PostScrap postScrap, Boolean isLiked) {
+        Post post = postScrap.getPost();
+
+        return PostScrapGetResponse.builder()
+                .postScrapId(postScrap.getPostScrapId())
+                .postId(post.getPostId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .isPublic(post.getIsPublic())
+                .isLiked(isLiked)
+                .totalLike(post.getTotalLike())
+                .isScrapped(true)
+                .totalScrap(post.getTotalScrap())
+                .totalComment(post.getTotalComment())
+                .updatedAt(TimeUtil.getUpdatedAt(post.getUpdatedAt()))
+                .build();
+    }
+}
