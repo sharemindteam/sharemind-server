58e534457ba8f0cc442bff843c5d2b4a0d788022
letskuku <yujung9288@naver.com>
2024-03-28 01:22:48 +0900

    #157 feat: 특정 회원의 일대다 상담 스크랩 목록 조회하는 비즈니스 로직 구현

--- src/main/java/com/example/sharemind/postScrap/application/PostScrapService.java
+++ src/main/java/com/example/sharemind/postScrap/application/PostScrapService.java
@@ -1,8 +1,13 @@
 package com.example.sharemind.postScrap.application;
 
+import com.example.sharemind.postScrap.dto.response.PostScrapGetResponse;
+import java.util.List;
+
 public interface PostScrapService {
 
     void createPostScrap(Long postId, Long customerId);
 
     void deletePostScrap(Long postId, Long customerId);
+
+    List<PostScrapGetResponse> getScrappedPosts(Long postScrapId, Long customerId);
 }
--- src/main/java/com/example/sharemind/postScrap/application/PostScrapServiceImpl.java
+++ src/main/java/com/example/sharemind/postScrap/application/PostScrapServiceImpl.java
@@ -4,10 +4,13 @@ import com.example.sharemind.customer.application.CustomerService;
 import com.example.sharemind.customer.domain.Customer;
 import com.example.sharemind.post.application.PostService;
 import com.example.sharemind.post.domain.Post;
+import com.example.sharemind.postLike.repository.PostLikeRepository;
 import com.example.sharemind.postScrap.domain.PostScrap;
+import com.example.sharemind.postScrap.dto.response.PostScrapGetResponse;
 import com.example.sharemind.postScrap.exception.PostScrapErrorCode;
 import com.example.sharemind.postScrap.exception.PostScrapException;
 import com.example.sharemind.postScrap.repository.PostScrapRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,9 +20,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class PostScrapServiceImpl implements PostScrapService {
 
+    private static final int POST_PAGE_SIZE = 6;
+
     private final CustomerService customerService;
     private final PostService postService;
     private final PostScrapRepository postScrapRepository;
+    private final PostLikeRepository postLikeRepository;
 
     @Transactional
     @Override
@@ -54,5 +60,17 @@ public class PostScrapServiceImpl implements PostScrapService {
                 .orElseThrow(() -> new PostScrapException(PostScrapErrorCode.POST_SCRAP_NOT_FOUND));
 
         postScrap.updateIsActivatedFalse();
+    }
+
+    @Override
+    public List<PostScrapGetResponse> getScrappedPosts(Long postScrapId, Long customerId) {
+        Customer customer = customerService.getCustomerByCustomerId(customerId);
+
+        return postScrapRepository.findAllByCustomerAndIsActivatedIsTrue(customer, postScrapId,
+                        POST_PAGE_SIZE).stream()
+                .map(postScrap -> PostScrapGetResponse.of(postScrap,
+                        postLikeRepository.existsByPostAndCustomerAndIsActivatedIsTrue(
+                                postScrap.getPost(), customer)))
+                .toList();
     }
 }
