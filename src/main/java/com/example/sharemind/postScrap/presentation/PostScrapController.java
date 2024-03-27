4414e05bf62e9c6a890136152bd0b162df6ba794
letskuku <yujung9288@naver.com>
2024-03-28 01:22:58 +0900

    #157 feat: 특정 회원의 일대다 상담 스크랩 목록 조회하는 api 구현

--- src/main/java/com/example/sharemind/postScrap/presentation/PostScrapController.java
+++ src/main/java/com/example/sharemind/postScrap/presentation/PostScrapController.java
@@ -3,6 +3,7 @@ package com.example.sharemind.postScrap.presentation;
 import com.example.sharemind.global.exception.CustomExceptionResponse;
 import com.example.sharemind.global.jwt.CustomUserDetails;
 import com.example.sharemind.postScrap.application.PostScrapService;
+import com.example.sharemind.postScrap.dto.response.PostScrapGetResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -11,14 +12,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "PostScrap Controller", description = "일대다 상담 질문 스크랩 컨트롤러")
@@ -71,5 +75,29 @@ public class PostScrapController {
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         postScrapService.deletePostScrap(postId, customUserDetails.getCustomer().getCustomerId());
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "일대다 상담 스크랩 리스트 조회", description = """
+            - 특정 회원이 스크랩한 일대다 상담 리스트 조회
+            - 주소 형식: /api/v1/postScraps&postScrapId=0""")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 회원",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomExceptionResponse.class))
+            )
+    })
+    @Parameters({
+            @Parameter(name = "postScrapId", description = """
+                    - 조회 결과는 6개씩 반환하며, postScrapId로 구분
+                    1. 최초 조회 요청이면 postScrapId는 0
+                    2. 2번째 요청부터 postScrapId는 직전 요청의 조회 결과 6개 중 마지막 postScrapId""")
+    })
+    @GetMapping
+    public ResponseEntity<List<PostScrapGetResponse>> getScrappedPosts(
+            @RequestParam Long postScrapId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(postScrapService.getScrappedPosts(postScrapId,
+                customUserDetails.getCustomer().getCustomerId()));
     }
 }
