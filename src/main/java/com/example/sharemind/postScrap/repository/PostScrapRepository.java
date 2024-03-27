7306ff7c1ea05e4bd2e5e2408bea4e53a07ab823
letskuku <yujung9288@naver.com>
2024-03-28 01:22:32 +0900

    #157 feat: 특정 회원의 일대다 상담 스크랩 목록 조회 위한 queryDsl 로직 구현

--- src/main/java/com/example/sharemind/postScrap/repository/PostScrapRepository.java
+++ src/main/java/com/example/sharemind/postScrap/repository/PostScrapRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostScrapRepository extends JpaRepository<PostScrap, Long> {
+public interface PostScrapRepository extends JpaRepository<PostScrap, Long>,
+        PostScrapCustomRepository {
 
     Boolean existsByPostAndCustomer(Post post, Customer customer);
 
