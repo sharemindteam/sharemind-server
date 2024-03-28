4c779aef3f65f580a4aa01958df9d9be95003538
letskuku <yujung9288@naver.com>
2024-03-29 01:39:12 +0900

    #157 feat: 스크랩순으로 스크랩 목록 가져오도록 queryDsl 로직 수정

--- src/main/java/com/example/sharemind/postScrap/repository/PostScrapCustomRepository.java
+++ src/main/java/com/example/sharemind/postScrap/repository/PostScrapCustomRepository.java
@@ -2,10 +2,11 @@ package com.example.sharemind.postScrap.repository;
 
 import com.example.sharemind.customer.domain.Customer;
 import com.example.sharemind.postScrap.domain.PostScrap;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostScrapCustomRepository {
 
     List<PostScrap> findAllByCustomerAndIsActivatedIsTrue(Customer customer, Long postScrapId,
-            int size);
+           LocalDateTime updatedAt, int size);
 }
--- src/main/java/com/example/sharemind/postScrap/repository/PostScrapCustomRepositoryImpl.java
+++ src/main/java/com/example/sharemind/postScrap/repository/PostScrapCustomRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.example.sharemind.postScrap.domain.PostScrap;
 import com.example.sharemind.postScrap.domain.QPostScrap;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
@@ -16,17 +17,20 @@ public class PostScrapCustomRepositoryImpl implements PostScrapCustomRepository 
 
     @Override
     public List<PostScrap> findAllByCustomerAndIsActivatedIsTrue(Customer customer,
-            Long postScrapId, int size) {
+            Long postScrapId, LocalDateTime updatedAt, int size) {
         return jpaQueryFactory
                 .selectFrom(postScrap)
                 .where(
                         postScrap.customer.eq(customer),
                         postScrap.isActivated.isTrue(),
-                        lessThanPostScrapId(postScrapId)
-                ).orderBy(postScrap.postScrapId.desc()).limit(size).fetch();
+                        lessThanUpdatedAtAndPostScrapId(postScrapId, updatedAt)
+                ).orderBy(postScrap.updatedAt.desc(), postScrap.postScrapId.desc()).limit(size).fetch();
     }
 
-    private BooleanExpression lessThanPostScrapId(Long postScrapId) {
-        return postScrapId != 0 ? postScrap.postScrapId.lt(postScrapId) : null;
+    private BooleanExpression lessThanUpdatedAtAndPostScrapId(Long postScrapId,
+            LocalDateTime updatedAt) {
+        return postScrapId != 0 ? postScrap.updatedAt.lt(updatedAt)
+                .or(postScrap.updatedAt.eq(updatedAt).and(postScrap.postScrapId.lt(postScrapId)))
+                : null;
     }
 }
