d1c056710cc8a5b14610ab9dd16b354fe592c2ae
aeyongdodam <kikititi0510@gmail.com>
2024-03-31 22:48:47 +0900

    #164 feat: 검색 결과 저장 및 리턴 로직 생성

--- src/main/java/com/example/sharemind/searchWord/application/SearchWordService.java
+++ src/main/java/com/example/sharemind/searchWord/application/SearchWordService.java
@@ -1,22 +1,30 @@
 package com.example.sharemind.searchWord.application;
 
 import com.example.sharemind.counselor.dto.response.CounselorGetListResponse;
+import com.example.sharemind.post.dto.response.PostGetListResponse;
 import com.example.sharemind.searchWord.dto.request.SearchWordDeleteRequest;
-import com.example.sharemind.searchWord.dto.request.SearchWordFindRequest;
+import com.example.sharemind.searchWord.dto.request.SearchWordCounselorFindRequest;
 
+import com.example.sharemind.searchWord.dto.request.SearchWordPostFindRequest;
 import java.util.List;
 
 public interface SearchWordService {
 
     List<CounselorGetListResponse> storeSearchWordAndGetCounselorsByCustomer(Long customerId, String sortType,
-                                                                             SearchWordFindRequest searchWordFindRequest);
+                                                                             SearchWordCounselorFindRequest searchWordCounselorFindRequest);
 
     List<CounselorGetListResponse> storeAllSearchWordAndGetCounselors(String sortType,
-                                                                      SearchWordFindRequest searchWordFindRequest);
+                                                                      SearchWordCounselorFindRequest searchWordCounselorFindRequest);
 
     List<String> getRecentSearchWordsByCustomer(Long customerId);
 
     void removeSearchWordByCustomer(Long customerId, SearchWordDeleteRequest searchWordDeleteRequest);
 
     void storeSearchWordInDB(String word);
+
+    List<PostGetListResponse> storeAllSearchWordAndGetPosts(String sortType,
+                                                            SearchWordPostFindRequest searchWordPostFindRequest);
+
+    List<PostGetListResponse> storeSearchWordAndGetPosts(Long customerId, String sortType,
+                                                         SearchWordPostFindRequest searchWordPostFindRequest);
 }
