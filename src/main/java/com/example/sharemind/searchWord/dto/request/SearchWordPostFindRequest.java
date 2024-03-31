183e55c79116aca93e6d9721f1324c17ca3afec1
aeyongdodam <kikititi0510@gmail.com>
2024-03-31 22:42:02 +0900

    #164 feat: Post 검색을 위한 request 생성

---
+++ src/main/java/com/example/sharemind/searchWord/dto/request/SearchWordPostFindRequest.java
@@ -0,0 +1,16 @@
+package com.example.sharemind.searchWord.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class SearchWordPostFindRequest {
+
+    @Schema(description = "검색어")
+    @Size(min = 2, max = 20, message = "검색어는 2-20글자 사이에서 입력 가능합니다.")
+    private String word;
+
+    @Schema(description = "가장 마지막으로 받은 postId 반환, 처음 요청이라면 0을 반환", example = "0")
+    private Long postId;
+}
