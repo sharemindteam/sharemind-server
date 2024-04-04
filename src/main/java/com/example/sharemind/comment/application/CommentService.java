c480b86e57ee355520a35182dd65aff27138a43a
aeyongdodam <kikititi0510@gmail.com>
2024-04-04 18:07:31 +0900

    #165 feat: 답변 채택 로직 생성

--- src/main/java/com/example/sharemind/comment/application/CommentService.java
+++ src/main/java/com/example/sharemind/comment/application/CommentService.java
@@ -14,4 +14,6 @@ public interface CommentService {
     void createComment(CommentCreateRequest commentCreateRequest, Long customerId);
 
     Comment getCommentByCommentId(Long commentId);
+
+    void updateCustomerChosenComment(Long postId, Long commentId, Long customerId);
 }
