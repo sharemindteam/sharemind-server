9ac7ed47bca27e885ec8e36dfb526cda50a015fc
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2024-04-25 17:39:57 +0900

    chore: 서버 타임존 설정

--- src/main/java/com/example/sharemind/SharemindApplication.java
+++ src/main/java/com/example/sharemind/SharemindApplication.java
@@ -1,5 +1,7 @@
 package com.example.sharemind;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,4 +16,8 @@ public class SharemindApplication {
 		SpringApplication.run(SharemindApplication.class, args);
 	}
 
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
