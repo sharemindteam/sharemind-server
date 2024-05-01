0aa1bdd84c83f3694fd9d46c5f7eca964838d767
aeyongdodam <kikititi0510@gmail.com>
2024-05-01 17:08:23 +0900

    #181 fix: 상담사일 시 메세지 내용 변경

--- src/main/java/com/example/sharemind/chatMessage/utils/ChatMessageUtil.java
+++ src/main/java/com/example/sharemind/chatMessage/utils/ChatMessageUtil.java
@@ -3,7 +3,11 @@ package com.example.sharemind.chatMessage.utils;
 import com.example.sharemind.chat.domain.Chat;
 
 public class ChatMessageUtil {
-    public static String getFinishMessage(Chat chat, String content) {
+    private static final String COUNSELOR_CHAT_FINISH = "님과 상담이 종료되었어요.";
+    public static String getFinishMessage(Chat chat, String content, Boolean isCustomer) {
+        if (!isCustomer) {
+            return chat.getConsult().getCustomer().getNickname() + COUNSELOR_CHAT_FINISH;
+        }
         String nickname = chat.getConsult().getCounselor().getNickname();
         return nickname + content;
     }
