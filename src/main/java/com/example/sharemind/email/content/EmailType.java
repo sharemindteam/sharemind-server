7d3c36680b82c0491f0c4919074c2847b9bdbd14
letskuku <yujung9288@naver.com>
2024-05-15 21:04:07 +0900

    #185 feat: 상담사 판매정보 검토 메일 텍스트 추가

--- src/main/java/com/example/sharemind/email/content/EmailType.java
+++ src/main/java/com/example/sharemind/email/content/EmailType.java
@@ -15,12 +15,24 @@ public enum EmailType {
                     """),
     FIND_ID("sharemind 아이디입니다.",
             """
-                    sharemind 아이디 정보 입니다.
+                    sharemind 아이디 정보입니다.
                     """),
     FIND_PASSWORD("sharemind 임시 비밀번호입니다.",
             """
                     sharemind 임시 비밀번호입니다.
                     사이트에 접속하여 새로운 비밀번호로 변경해주세요.
+                    """),
+    COUNSELOR_PROFILE_COMPLETE("sharemind 마인더 판매정보 검토 결과 안내드립니다.",
+            """
+                    안녕하세요, sharemind 입니다.
+                    마인더 판매정보 검토 결과, 판매정보 수정이 승인되었습니다.
+                    지금부터 수정된 판매정보로 마인더 활동이 가능합니다.
+                    """),
+    COUNSELOR_PROFILE_FAIL("sharemind 마인더 판매정보 검토 결과 안내드립니다.",
+            """
+                    안녕하세요, sharemind 입니다.
+                    마인더 판매정보 검토 결과, 판매정보 수정이 반려되었습니다.
+                    판매정보를 다시 수정해주세요.
                     """);
 
     private final String subject;
