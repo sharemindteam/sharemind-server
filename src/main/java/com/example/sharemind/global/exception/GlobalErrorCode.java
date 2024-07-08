d5e629ec23246df3e4928e444cd93596ebbca7ba
aeyongdodam <kikititi0510@gmail.com>
2024-07-09 00:54:33 +0900

    #211 feat: encrypted 관련 에러 코드 생성

--- src/main/java/com/example/sharemind/global/exception/GlobalErrorCode.java
+++ src/main/java/com/example/sharemind/global/exception/GlobalErrorCode.java
@@ -8,7 +8,8 @@ public enum GlobalErrorCode {
 
     CONSULT_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "상담 카테고리가 존재하지 않습니다."),
     CONSULT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "상담 유형이 존재하지 않습니다."),
-    CONSULT_SORT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, " 해당 정렬 방식이 존재하지 않습니다.");
+    CONSULT_SORT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, " 해당 정렬 방식이 존재하지 않습니다."),
+    INVALID_ID(HttpStatus.BAD_REQUEST, "잘못된 아이디입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
