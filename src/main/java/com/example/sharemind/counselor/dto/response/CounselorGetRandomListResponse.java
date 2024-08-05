3982053d2ae4b405727b7887957da571a2abb349
aeyongdodam <kikititi0510@gmail.com>
2024-08-06 01:03:49 +0900

    #224 fix: CounselorGetRandomListResponse 생성

---
+++ src/main/java/com/example/sharemind/counselor/dto/response/CounselorGetRandomListResponse.java
@@ -0,0 +1,23 @@
+package com.example.sharemind.counselor.dto.response;
+
+import com.example.sharemind.counselor.domain.Counselor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class CounselorGetRandomListResponse extends CounselorGetListResponse {
+
+    @Schema(description = "정렬 기준", example = "rating")
+    private final String sortType;
+
+    private CounselorGetRandomListResponse(Counselor counselor, Boolean isWishList, Boolean isRealtime,
+                                           String sortType) {
+        super(counselor, isWishList, isRealtime);
+        this.sortType = sortType;
+    }
+
+    public static CounselorGetRandomListResponse of(Counselor counselor, Boolean isWishList, Boolean isRealtime,
+                                                    String sortType) {
+        return new CounselorGetRandomListResponse(counselor, isWishList, isRealtime, sortType);
+    }
+}
