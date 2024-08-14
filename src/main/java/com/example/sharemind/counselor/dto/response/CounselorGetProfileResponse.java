976b61c9760a1d117aaca3eff060e7a8dba4b0c5
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2024-08-14 20:20:52 +0900

    hotfix: 상담사 전화번호 누락된 dto 추가

--- src/main/java/com/example/sharemind/counselor/dto/response/CounselorGetProfileResponse.java
+++ src/main/java/com/example/sharemind/counselor/dto/response/CounselorGetProfileResponse.java
@@ -17,10 +17,14 @@ public class CounselorGetProfileResponse extends CounselorGetBaseResponse {
     @Schema(description = "경험 소개")
     private final String experience;
 
+    @Schema(description = "전화번호")
+    private final String phoneNumber;
+
     private CounselorGetProfileResponse(Counselor counselor) {
         super(counselor);
         this.counselorId = counselor.getCounselorId();
         this.experience = counselor.getExperience();
+        this.phoneNumber = counselor.getPhoneNumber();
     }
 
     public static CounselorGetProfileResponse of(Counselor counselor) {
