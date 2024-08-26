73608c016ccf5c8158cf324bdb97867408810f38
aeyongdodam <kikititi0510@gmail.com>
2024-08-27 01:52:42 +0900

    #247 fix: Sms 전송 로직 수정

--- src/main/java/com/example/sharemind/sms/application/SmsService.java
+++ src/main/java/com/example/sharemind/sms/application/SmsService.java
@@ -1,9 +1,8 @@
 package com.example.sharemind.sms.application;
 
 import com.example.sharemind.sms.dto.response.SmsGetResponse;
-import reactor.core.publisher.Mono;
 
 public interface SmsService {
 
-    public Mono<String> sendSms(String phoneNumber);
+    SmsGetResponse sendSmsCounselor(Long counselorId);
 }
--- src/main/java/com/example/sharemind/sms/application/SmsServiceImpl.java
+++ src/main/java/com/example/sharemind/sms/application/SmsServiceImpl.java
@@ -1,19 +1,27 @@
 package com.example.sharemind.sms.application;
 
+import com.example.sharemind.counselor.application.CounselorService;
+import com.example.sharemind.counselor.domain.Counselor;
 import com.example.sharemind.sms.dto.response.SmsGetResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SmsServiceImpl implements SmsService {
+
+    private static final String BASE_URL = "https://apis.aligo.in";
+    private static final String COUNSELOR_MESSAGE = "새로운 상담 신청이 도착했습니다.";
+
+    private final CounselorService counselorService;
 
     @Value("${spring.sms.key}")
     private String key;
@@ -24,26 +32,38 @@ public class SmsServiceImpl implements SmsService {
     @Value("${spring.sms.sender}")
     private String sender;
 
-    private final WebClient webClient;
+    private final RestClient restClient = RestClient.builder()
+            .baseUrl(BASE_URL)
+            .build();
+    private final ObjectMapper objectMapper;
 
     @Override
-    public Mono<String> sendSms(String phoneNumber) {
-        return webClient.post()
+    public SmsGetResponse sendSmsCounselor(Long counselorId) {
+        Counselor counselor = counselorService.getCounselorByCounselorId(counselorId);
+        return sendSms(counselor.getPhoneNumber(), COUNSELOR_MESSAGE);
+    }
+
+    private SmsGetResponse sendSms(String phoneNumber, String message) {
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("key", key);
+        body.add("user_id", id);
+        body.add("sender", sender);
+        body.add("receiver", phoneNumber);
+        body.add("msg", message);
+//        body.add("testmode_yn", "Y");
+
+        String response = restClient.post()
                 .uri("/send/")
-                .body(BodyInserters.fromFormData("key", key)
-                        .with("user_id", id)
-                        .with("sender", sender)
-                        .with("receiver", "")
-                        .with("msg", "테스트입니다")
-//                        .with("testmode_yn", "Y")
-                ).retrieve()
-                .bodyToMono(String.class)
-                .doOnNext(response -> {
-                    System.out.println("Received response from API: " + response);
-                })
-                .onErrorResume(WebClientResponseException.class, ex -> {
-                    System.out.println("Error calling API: " + ex.getMessage());
-                    return Mono.error(new RuntimeException("API 호출 실패: " + ex.getMessage()));
-                });
+                .body(body).retrieve()
+                .body(String.class);
+        try {
+            JsonNode rootNode = objectMapper.readTree(response);
+            int resultCode = rootNode.get("result_code").asInt();
+            String responseMessage = rootNode.get("message").asText();
+            return SmsGetResponse.of(resultCode, responseMessage);
+        } catch (Exception e) {
+            throw new RuntimeException("JSON 처리 중 오류 발생", e);
+        }
     }
 }
