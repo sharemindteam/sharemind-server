d0ad2885c050789cb9e935d486404393907f9b6f
aeyongdodam <kikititi0510@gmail.com>
2024-08-27 01:49:52 +0900

    #247 fix: SmsGetResponse 필요한 정보만 dto에 담아서 반환

--- src/main/java/com/example/sharemind/sms/dto/response/SmsGetResponse.java
+++ src/main/java/com/example/sharemind/sms/dto/response/SmsGetResponse.java
@@ -10,17 +10,8 @@ public class SmsGetResponse {
 
     private final int result_code;
     private final String message;
-    private final Integer msg_id;
-    private final Integer success_cnt;
-    private final Integer error_cnt;
-    private final String msg_type;
-
-    public static SmsGetResponse of(int resultCode, String message, Integer msgId,
-            Integer successCnt, Integer errorCnt, String msgType) {
-        return new SmsGetResponse(resultCode, message, msgId, successCnt, errorCnt, msgType);
-    }
 
     public static SmsGetResponse of(int resultCode, String message) {
-        return new SmsGetResponse(resultCode, message, null, 0, 1, null);
+        return new SmsGetResponse(resultCode, message);
     }
 }
